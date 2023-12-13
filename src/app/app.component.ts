021720f34e1d2b3df1df65caabc3298d13aaee5e
Raissa Scheeren <raissasche@gmail.com>
2023-12-12 22:02:21 -0300

    comunicação básica com a api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Constants } from './config/constants';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ApiHttpService } from './core/services/api-http.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'poetry-of-the-day';
+  title = Constants.TitleOfSite;
+  constructor() {
+    const api = inject(ApiHttpService);
+    console.log(Constants.API_ENDPOINT);
+    api.get('https://poetrydb.org/random/1/title').subscribe((resp) => {
+      console.log(resp);
+    });
+
+    console.log(this.title);
+  }
+
+  ngOnInit() {
+    console.log(this.title);
+  }
 }
---
+++ src/app/config/constants.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Constants {
+  public static API_ENDPOINT: string = 'https://poetrydb.org/';
+  public static TitleOfSite: string = ' Poetry of The Day';
+}
